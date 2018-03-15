dae701587cae8fd224762a5fb41182c7f1289849
leon082 <leon9326@gmail.com>
2018-03-15 12:21:52 -0500

    Para pruebas 20180314

--- Pruebas/src/nodos/listasenlazadas/Pruebas.java
+++ Pruebas/src/nodos/listasenlazadas/Pruebas.java
@@ -6,6 +6,8 @@
 package nodos.listasenlazadas;
 
 import com.sun.xml.internal.ws.developer.UsesJAXBContext;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.text.SimpleDateFormat;
@@ -61,7 +63,37 @@ public class Pruebas {
         int x2 = in.nextInt();
         int v2 = in.nextInt();
         String result = kangaroo(x1, v1, x2, v2);
-        System.out.println(result);*/
+        System.out.println(result);
+          Scanner in = new Scanner(System.in);
+        int n = Integer.valueOf(in.nextLine());
+        
+        String[] cadenas = new String[n];
+       
+        for(int i=0; i<n;i++){
+            
+            cadenas[i]=in.nextLine();
+        }
+       
+               int m = Integer.valueOf(in.nextLine());
+         String[] queries = new String[m];
+        
+        for(int i=0; i<m;i++){
+            queries[i]=in.nextLine();
+        }
+        int contador=0;
+        for(int i=0; i< queries.length;i++){
+            for(int j=0; j< cadenas.length;j++){
+                    
+                if(queries[i].equalsIgnoreCase(cadenas[j])){
+                    contador++;
+                }
+                    
+                
+            }
+            System.out.println(contador);
+            contador=0;
+        }
+        
     }
     /*
     public Node Reverse(Node head) {
@@ -80,7 +112,62 @@ public class Pruebas {
    
     }
     */
+   /*Node InsertNth(Node head, int data, int position) {
+   // This is a "method-only" submission. 
+    // You only need to complete this method. 
+    Node prev;
+        Node current= head;
+        Node element= new Node(data,null);
+        Node temp;
+        int index=0;
+        while(current != null ){
+            
+            temp=current;
+            current = current.next;           
+            prev = temp;
+                 if(index == position){
+                      prev.next=element;
+                      element.next=current;
+                      break;
+            
+            }
+            index++;
+            
+        }
+        
+        return head;
     
+}
+   
+  Scanner in = new Scanner(System.in);
+        double meal_cost = in.nextDouble();
+        int tip_percent = in.nextInt();
+        int tax_percent = in.nextInt();
+        in.close();
+        double tip = meal_cost*( (float) tip_percent/100);
+       
+        double tax = meal_cost*((float) tax_percent/100);
+        System.out.println(tax);
+        double total= meal_cost+tip+tax;
+        System.out.println(total);
+        int imprimir= (int) Math.round (total);
+        System.out.println("The total meal cost is "+imprimir+" dollars.");*/
+   
+   int a=5;
+   int b=2;
+   
+   
+   a= a+b;   
+   b= a-b;   
+   a=a-b;
+   
+   System.out.println("Valor de A= "+a);
+   System.out.println("Valor de B= "+b);
+   
+   
+   
+   
+    }
         static String kangaroo(int x1, int v1, int x2, int v2) {
              // Complete this function
              boolean flag=true;
@@ -110,6 +197,7 @@ public class Pruebas {
             return respuesta;
              
     }
+        
     public static void init(){
         //System.out.println(prueba.saludo());
     }
